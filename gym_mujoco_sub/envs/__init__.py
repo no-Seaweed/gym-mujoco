585c27a75a16ea0c924e5dae663095f4050376e4
no-Seaweed <40819442+no-Seaweed@users.noreply.github.com>
2019-04-17 22:16:46 -0400

    Add files via upload
    Initializing project

---
+++ gym_mujoco_sub/envs/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 17 21:53:24 2019
+
+@author: Shukai
+"""
+
+from gym_foo.envs.mujoco_env import MujocoEnv
+#from gym_foo.envs.foo_extrahard_env import FooExtraHardEnv
